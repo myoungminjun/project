d14cd36c2d11c9f4b34c9e2fd8a2f5f1fa0f4e59
myoungminjun <93511456+myoungminjun@users.noreply.github.com>
2021-11-24 16:59:41 +0900

    Delete WebContent directory

--- WebContent/project.sql
+++
@@ -1,5 +0,0 @@
-select * from mem
-insert into mem (m_id,m_pw,m_name,m_email,m_tel,m_zip,m_addr1,m_addr2,regdate) values('admin','1234','관리자','admin@naver.com','null','null','null','null',sysdate)
-alter table mem add name varchar2(20)
-select * from product
-select * from notice
